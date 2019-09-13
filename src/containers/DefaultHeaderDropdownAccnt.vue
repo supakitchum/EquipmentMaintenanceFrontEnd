16e7e38754179ecda65dcb4ee6aba110a6a89989
Supakit <supakit_c@cmu.ac.th>
2019-09-13 16:02:28 +0700

    ีupdate when finished go back

--- src/containers/DefaultHeaderDropdownAccnt.vue
+++ src/containers/DefaultHeaderDropdownAccnt.vue
@@ -5,7 +5,7 @@
         src="img/avatars/6.jpg"
         class="img-avatar"
         alt="admin@bootstrapmaster.com" />
-    </template>\
+    </template>
     <template slot="dropdown">
       <b-dropdown-item :href="`#/users/profile`"><i class="fa fa-user" /> ข้อมูลส่วนตัว</b-dropdown-item>
       <b-dropdown-item @click="logout"><i class="fa fa-lock" /> ออกจากระบบ</b-dropdown-item>
